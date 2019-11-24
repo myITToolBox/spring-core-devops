f4e2f0150ed2f47636944d9dafd11e98368f92f7
Alessandro Behling <sandro.extremesports@gmail.com>
2019-11-24 01:30:12 +0000

    Added some changes - testing CI build

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,7 +9,7 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		System.out.println("Testing build 2");
+		System.out.println("Testing build 20");
 		//this is a whitespace change test
 		// another test...
 	}
